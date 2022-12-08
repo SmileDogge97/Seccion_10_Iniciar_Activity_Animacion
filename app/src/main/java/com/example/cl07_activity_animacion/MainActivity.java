f00bfa07f4cd13a6fcb856ff5d0a914322a113a0
José Francisco Jarquin Martínez <ex500791.jose.jarquin@mail.telcel.com>
2022-12-08 10:45:02 -0600

    el proyecto vacío

---
+++ app/src/main/java/com/example/cl07_activity_animacion/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cl07_activity_animacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
