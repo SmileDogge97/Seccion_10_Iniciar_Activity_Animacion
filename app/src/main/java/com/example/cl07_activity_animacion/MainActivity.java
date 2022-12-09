51a63c4af1eeef7fd03b3b574b57372084b718c5
José Francisco Jarquin Martínez <ex500791.jose.jarquin@mail.telcel.com>
2022-12-09 11:45:13 -0600

    proyecto terminado

--- app/src/main/java/com/example/cl07_activity_animacion/MainActivity.java
+++ app/src/main/java/com/example/cl07_activity_animacion/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cl07_activity_animacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void navegar(View view){
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this);
+        Intent i = new Intent(this, SecundarioActivity.class);
+        startActivity(i, options.toBundle());
     }
 }
---
+++ app/src/main/java/com/example/cl07_activity_animacion/SecundarioActivity.java
@@ -0,0 +1,17 @@
+package com.example.cl07_activity_animacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecundarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secundario);
+    }
+
+}
